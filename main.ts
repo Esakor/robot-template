fda12d16657b8b5e8e244e245e688f9a71f383bb
Esakor <100299697+Esakor@users.noreply.github.com>
2022-10-17 14:41:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 function Turn_Right () {
     pins.servoSetPulse(AnalogPin.P8, 0)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
 function Sensor () {
@@ -9,20 +10,24 @@ function Sensor () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    if (distance < 5) {
+        pins.servoSetPulse(AnalogPin.P8, 0)
+        pins.servoSetPulse(AnalogPin.P13, 0)
+    }
 }
 function Forward () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
 function Backward2 () {
-    pins.servoSetPulse(AnalogPin.P13, 1700)
-    control.waitMicros(20000)
+	
 }
 input.onButtonPressed(Button.A, function () {
     Forward()
-    Forward2()
 })
 function Turn_Left () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
     pins.servoSetPulse(AnalogPin.P13, 0)
     control.waitMicros(20000)
 }
@@ -31,22 +36,22 @@ input.onButtonPressed(Button.AB, function () {
     Turn_Right()
 })
 function Forward2 () {
-    pins.servoSetPulse(AnalogPin.P13, 1300)
-    control.waitMicros(20000)
+	
 }
 input.onButtonPressed(Button.B, function () {
     Backward2()
-    Backward()
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-	
+    Turn_Left()
+    Forward()
 })
 function Backward () {
-    pins.servoSetPulse(AnalogPin.P8, 1300)
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
 let distance = 0
-basic.showIcon(IconNames.No)
+basic.showIcon(IconNames.Ghost)
 distance = 0
 basic.forever(function () {
     Sensor()
